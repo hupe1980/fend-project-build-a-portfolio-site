8a55e3e593c4a9988be411c7e8b98f957af55310
hupe1980 <hupe1980@users.noreply.github.com>
2018-02-18 16:59:37 +0100

    feat: Add project description

--- src/App.js
+++ src/App.js
@@ -8,16 +8,22 @@ const projects = [
   {
     name: 'Appify',
     imgSrc: `${process.env.PUBLIC_URL}/img/appify.jpg`,
+    desc:
+      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
     link: 'https://github.com/udacity/Appify/',
   },
   {
     name: 'Sunflower',
     imgSrc: `${process.env.PUBLIC_URL}/img/sunflower.jpg`,
+    desc:
+      ' Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
     link: 'https://github.com/udacity/Sunflower/',
   },
   {
     name: 'Bokeh',
     imgSrc: `${process.env.PUBLIC_URL}/img/bokeh.jpg`,
+    desc:
+      'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
     link: 'https://github.com/udacity/Bokeh/',
   },
 ];
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 import './Project.css';
 
-export default ({ name, imgSrc, link }) => (
-  <div className="col-md-4 col-sm-6 col-xs-12">
+export default ({ name, imgSrc, desc, link }) => (
+  <div className="col-md-4 col-sm-6 col-xs-12 mb-3">
     <div className="card">
       <img className="card-img-top" src={imgSrc} alt={name} />
       <div className="card-body text-center">
         <h3 className="text-uppercase">{name}</h3>
+        <p class="card-text">{desc}</p>
         <a href={link} className="card-link project_link">
           {link}
         </a>
